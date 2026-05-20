e94b2de7e1b239670cf30ebb99e91ed93b64bde8
AydaSelen Pilanci <aydaselen.pilanci@gmail.com>
2026-05-20 15:06:26 +0900

    Solved the chat save issueee

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,16 @@
 class MessagesController < ApplicationController
   def create
-    @message = Message.new(message_params)
+    @deed = Deed.find(params[:deed_id])
+    @message = @deed.messages.new(message_params)
+    @message.role = "user"
     raise unless @message.save
 
     redirect_to deed_path(@message.deed)
+  end
+
+  def show
+    @deed = Deed.find(params[:id])
+    @message = Message.new
   end
 
   private
