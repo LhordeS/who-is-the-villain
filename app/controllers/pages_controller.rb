dcc27b949fc5140d9609de413de869f19a09d878
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-05-21 11:46:29 +0900

    added content to the homepage view including a post link, deeds link, and the top 3 voted on deeds and updated pages controller to include home definition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @public_deeds = Deed.all.select { |deed| deed.public == true }
+    @sorted_deeds = @public_deeds.sort_by { |deed| deed.votes_for.size }
+    @deeds = @sorted_deeds.last(3).reverse
   end
 
   def dashboard
